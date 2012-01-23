5e29cb8466908594ac22b1db66e4cfaabd5e7185
talleyberry <talleyberry@gmail.com>
2012-01-23 18:34:27 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dartsight::Application.config.secret_token = 'ab37d683e14b67ac546aeda6beb744057933b8123b8ae71bfceea29a0974dbbb3a47c44f917ac7e277189c27141e0fac5c75f90f3d2d0d3c5305d50c7206e260'
